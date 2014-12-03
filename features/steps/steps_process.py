547de045990a93064d6f788693b5a71b0db1ed76
grierson <grierson@openmailbox.org>
2014-12-03 22:19:56 +0000

    Step File

---
+++ features/steps/steps_process.py
@@ -0,0 +1,16 @@
+@given('I enter Firefox')
+def step_impl(context):
+    assert False
+
+@given('I enter 100')
+def step_impl(context):
+    assert False
+
+@when('I press create process')
+def step_impl(context):
+    assert False
+
+@then('a process with Firefox and 100 should be created')
+def step_impl(context):
+    assert False
+
