2adca668a12b7d19b626417a9ff88a0200b9a7b9
grierson <grierson@openmailbox.org>
2015-01-19 15:58:37 +0000

    __init__.py

--- memsim/process.py
+++ memsim/process.py
@@ -8,24 +8,28 @@ class Process:
 
         :param parent:
         """
-        self.frame = tk.LabelFrame(parent, text="Process", padx=5, pady=5)
-        self.frame.pack()
+        self.frame = tk.LabelFrame(parent,
+                                   text="Create Process",
+                                   padx=5,
+                                   pady=5)
+        self.frame.pack(anchor="nw", padx=5, pady=5)
+
+        # Process Name
         tk.Label(self.frame, text="Process Name: ").pack()
         self.entry_name = tk.Entry(self.frame)
-        self.entry_name.pack()
+        self.entry_name.pack(pady=5)
 
+        # Process Size
         tk.Label(self.frame, text="Process Size: ").pack()
         self.entry_size = tk.Entry(self.frame)
-        self.entry_size.pack()
+        self.entry_size.pack(pady=5)
 
+        # Create Process Button
         tk.Button(self.frame,
                   text="Create Process",
-                  command=self.get_process,
-                  padx=5,
-                  pady=5).pack()
+                  command=self.get_process).pack()
 
 
     def get_process(self):
         """get_process"""
-        print(self.entry_name.get())
-        print(self.entry_size.get())
+        return self.entry_name.get(), self.entry_size.get()
--- test/test_process.py
+++ test/test_process.py
@@ -1,9 +1,18 @@
 """ Test Process Creation """
 import unittest
-import process
+from mock import Mock
+from memsim.process import Process
 
 class TestProcess(unittest.TestCase):
     """Test_Process"""
     def test_create_process(self):
         """test_create_process"""
-        process = Process("Firefox", 500)
+        process = Process(mock.ANY)
+        p_name, p_size = process.get_process()
+        if p_name.isaplha() and p_size.isnum():
+            self.assertTrue(True)
+        else:
+            self.assertFalse(False)
+
+if __name__ == '__main__':
+    unittest.main()
