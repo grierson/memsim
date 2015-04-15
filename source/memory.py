9e12173e69881d78737006d99a8cc2acc47126b2
Kyle Grierson <griersonky@gmail.com>
2015-04-15 22:19:53 +0100

    Fixed Hole Label when Top Process is removed

--- source/memory.py
+++ source/memory.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Memory Canvas """
+''' Memory Canvas '''
 try:
     import tkinter as tk
     import tkinter.messagebox as messagebox
@@ -13,19 +13,19 @@ M_HIGHT = 450
 
 
 class Memory(tk.Canvas):
-    """Ram"""
+    '''Ram'''
     def __init__(self, parent):
-        """ (Tk.Frame) -> None
+        ''' (Tk.Frame) -> None
 
         Create Process List
-        """
+        '''
         tk.Canvas.__init__(self,
                            parent,
-                           bg="white",
+                           bg='white',
                            width=M_WIDTH,
                            height=M_HIGHT)
         self.processes = []
-        self.colours = ["red", "blue", "green", "cyan", "yellow", "magenta"]
+        self.colours = ['red', 'blue', 'green', 'cyan', 'yellow', 'magenta']
         self.find_holes()
         self.allocation_policies = [self.first_fit,
                                     self.best_fit,
@@ -33,82 +33,82 @@ class Memory(tk.Canvas):
         self.policy_selected = 1
 
     def selected_policy(self, value):
-        """ set self.selected_policies """
+        ''' set self.selected_policies '''
         if not isinstance(value, int):
             raise TypeError('Expected an int')
         self.policy_selected = value
 
     @property
     def process_list(self):
-        """ Get Process List """
-        return [process.get("name") for process in self.processes]
+        ''' Get Process List '''
+        return [process.get('name') for process in self.processes]
 
     def check_process_exists(self, process_name):
-        """ (string) -> Bool
+        ''' (string) -> Bool
 
         Check whether process name is in Process List
-        """
+        '''
         return any(True
                    for process in self.processes
-                   if process_name == process.get("name"))
+                   if process_name == process.get('name'))
 
     def get_process_size(self, process_name):
-        """ (string) -> int
+        ''' (string) -> int
 
         Get process size
-        """
-        return next(process.get("size")
+        '''
+        return next(process.get('size')
                     for process in self.processes
-                    if process_name == process.get("name"))
+                    if process_name == process.get('name'))
 
     def get_process_address(self, process_name):
-        """ (string) -> int
+        ''' (string) -> int
 
         Get process address
-        """
-        return next(process.get("address")
+        '''
+        return next(process.get('address')
                     for process in self.processes
-                    if process_name == process.get("name"))
+                    if process_name == process.get('name'))
 
     def validate_process(self, name, size):
-        """ (string, int) -> None
+        ''' (string, int) -> None
 
         Validate the users input is correct otherwise show error.
-        """
+        '''
         if not name.isalpha():
-            messagebox.showerror("Error!",
-                                 "Name must only contain letters")
+            messagebox.showerror('Error!',
+                                 'Name must only contain letters')
         elif not str(size).isdigit():
-            messagebox.showerror("Error!",
-                                 "Size must only contain numbers")
+            messagebox.showerror('Error!',
+                                 'Size must only contain numbers')
         elif int(size) <= 10:
-            messagebox.showerror("Error!",
-                                 "Size must be greater than 10")
+            messagebox.showerror('Error!',
+                                 'Size must be greater than 10')
         elif self.check_process_exists(name):
-            messagebox.showerror("Error!",
-                                 "Process Already Exists")
+            messagebox.showerror('Error!',
+                                 'Process Already Exists')
         else:
             self.allocation_policies[self.policy_selected](name, size)
 
     def create_process(self, name, size, address):
-        """ (string, int, int) -> Tk.Rectangle
+        ''' (string, int, int) -> Tk.Rectangle
 
         Create Tk.Rectangle which represents a Process
-        """
-        self.processes.append({"name": name,
-                               "size": size,
-                               "address": address,
-                               "colour": random.choice(self.colours)}.copy())
+        '''
+        self.processes.append({'name': name,
+                               'size': size,
+                               'address': address,
+                               'colour': random.choice(self.colours)}.copy())
         self.update_memory()
 
     def update_memory(self):
-        """ (string, int, int) -> Tk.Rectangle
+        ''' (string, int, int) -> Tk.Rectangle
 
         Create Tk.Rectangle which represents a Process
         [x1, y1, x2, y2]
-        """
+        '''
         # Clear Canvas
-        self.delete("all")
+        self.delete('all')
 
         # If Processes Empty then do nothing
         if len(self.processes) == 0:
@@ -118,52 +118,52 @@ class Memory(tk.Canvas):
         # Redraw Processes
         for process in self.processes:
             # TEMP variables to make code cleaner
-            name = process.get("name")
-            address = process.get("address")
-            size = process.get("size")
+            name = process.get('name')
+            address = process.get('address')
+            size = process.get('size')
 
             self.create_rectangle(0,
                                   address,
                                   M_WIDTH,
                                   address + size,
-                                  fill=process.get("colour"),
+                                  fill=process.get('colour'),
                                   width=1,
                                   tag=name)
             self.create_text((M_WIDTH / 2),
                              address + (size / 2),
-                             text="{} : {}".format(name, size))
+                             text='{} : {}'.format(name, size))
             self.create_text(22,
                              address + 6,
-                             text="{}".format(hex(address)))
+                             text='{}'.format(hex(address)))
 
         n_holes = 1
         for hole in self.find_holes():
-            if hole.get("size") == 0:
+            if hole.get('size') <= 1:
                 pass
             else:
                 self.create_text(M_WIDTH / 2,
-                                 hole.get("address") + (hole.get("size") / 2),
-                                 text="Hole {} : {}".format(n_holes,
-                                                            hole.get("size")))
+                                 hole.get('address') + (hole.get('size') / 2),
+                                 text='Hole {} : {}'.format(n_holes,
+                                                            hole.get('size')))
                 n_holes += 1
 
     def kill(self, process_name):
-        """ (string) -> None
+        ''' (string) -> None
 
         Kill process in Process list
-        """
+        '''
         self.processes[:] = [item
                              for item in self.processes
-                             if item.get("name") != process_name]
+                             if item.get('name') != process_name]
         self.update_memory()
 
-    """
+    '''
     -------------------------------
             Policies
     -------------------------------
-    """
+    '''
     def find_holes(self):
-        """ Find all Holes in Memory """
+        ''' Find all Holes in Memory '''
         address = 0
         hole_size = 0
         hole_address = 0
@@ -172,53 +172,47 @@ class Memory(tk.Canvas):
         if len(self.processes) <= 0:
             self.create_text(M_WIDTH / 2,
                              M_HIGHT / 2,
-                             text="Ram Size: {}".format(M_HIGHT))
-            return [{"address": 0, "size": M_HIGHT}]
+                             text='Ram Size: {}'.format(M_HIGHT))
+            return [{'address': 0, 'size': M_HIGHT}]
 
         while address <= M_HIGHT:
             for process in self.processes:
-                if address == process.get("address") or address == M_HIGHT:
-                    address += process.get("size")
-                    holes.append({"address": hole_address,
-                                  "size": hole_size})
-                    hole_size = 0
-                    hole_address = address
-                elif address == M_HIGHT:
-                    holes.append({"address": hole_address,
-                                  "size": hole_size})
+                if address == process.get('address') or address == M_HIGHT:
+                    address += process.get('size')
+                    holes.append({'address': hole_address,
+                                  'size': hole_size})
                     hole_size = 0
                     hole_address = address
             hole_size += 1
             address += 1
 
-        # Need to remove First Element which includes address: 0, size: 0
-        return holes[1:]
+        return sorted(holes, key=lambda hole: (hole['address']))
 
     def first_fit(self, process_name, process_size):
-        """ (string, int) -> None
+        ''' (string, int) -> None
 
         First Fit using find_holes
-        """
+        '''
         for hole in self.find_holes():
-            if hole.get("size") >= process_size:
+            if hole.get('size') >= process_size:
                 self.create_process(process_name,
                                     process_size,
-                                    hole.get("address"))
+                                    hole.get('address'))
                 return
 
     def best_fit(self, process_name, process_size):
-        """ (string, int) -> None
+        ''' (string, int) -> None
 
         Best Fit using find_holes
-        """
-        smallest_hole = max(hole.get("size") for hole in self.find_holes())
+        '''
+        smallest_hole = max(hole.get('size') for hole in self.find_holes())
         best_address = 0
 
         for hole in self.find_holes():
-            diff = hole.get("size") - process_size
+            diff = hole.get('size') - process_size
             if diff < smallest_hole and diff >= 0:
                 smallest_hole = diff
-                best_address = hole.get("address")
+                best_address = hole.get('address')
 
         self.create_process(process_name,
                             process_size,
@@ -227,28 +221,28 @@ class Memory(tk.Canvas):
         return
 
     def worst_fit(self, process_name, process_size):
-        """ (string, int) -> None
+        ''' (string, int) -> None
 
         Worst Fit using find_holes
-        """
-        biggest_hole = max(hole.get("size") for hole in self.find_holes())
+        '''
+        biggest_hole = max(hole.get('size') for hole in self.find_holes())
 
         for hole in self.find_holes():
-            if hole.get("size") == biggest_hole:
+            if hole.get('size') == biggest_hole:
                 self.create_process(process_name,
                                     process_size,
-                                    hole.get("address"))
+                                    hole.get('address'))
                 return
 
     def compact(self):
-        """ Compact """
+        ''' Compact '''
         for _ in range(len(self.processes)):
             prev_address = 0
             prev_size = 0
             for process in self.processes:
-                temp_address = process.get("address")
-                process["address"] = prev_address + prev_size
+                temp_address = process.get('address')
+                process['address'] = prev_address + prev_size
                 prev_address = temp_address
-                prev_size = process.get("size")  # Plus one so no overlap
+                prev_size = process.get('size')
 
         self.update_memory()
--- test/test_memory.py
+++ test/test_memory.py
@@ -1,55 +1,55 @@
-""" TEST MEMORY """
+''' TEST MEMORY '''
 from source.memory import Memory
 import tkinter
 import unittest
 
 
 class TestMemory(unittest.TestCase):
-    """ TEST Memory Class """
+    ''' TEST Memory Class '''
     def setUp(self):
-        """ Create Memory Panel """
+        ''' Create Memory Panel '''
         parent = tkinter.Tk()
         self.ram = Memory(parent)
 
     def test_check_process_exists(self):
-        """ Test that a process name is in the Process List """
-        self.ram.create_process("Style", 200, 0)
-        self.assertTrue(self.ram.check_process_exists("Style"))
+        ''' Test that a process name is in the Process List '''
+        self.ram.create_process('Style', 200, 0)
+        self.assertTrue(self.ram.check_process_exists('Style'))
 
     def test_get_process_size(self):
-        """ Test Return Process Size """
-        self.ram.create_process("Blankspace", 123, 0)
-        self.assertEqual(self.ram.get_process_size("Blankspace"), 123)
+        ''' Test Return Process Size '''
+        self.ram.create_process('Blankspace', 123, 0)
+        self.assertEqual(self.ram.get_process_size('Blankspace'), 123)
 
     def test_get_process_address(self):
-        """ Test Get Process Address
+        ''' Test Get Process Address
 
         Note: create_process(process_name, process_size, address)
-        """
-        self.ram.create_process("Red", 333, 111)
-        self.assertEqual(self.ram.get_process_address("Red"), 111)
+        '''
+        self.ram.create_process('Red', 333, 111)
+        self.assertEqual(self.ram.get_process_address('Red'), 111)
 
     def test_kill(self):
-        """ Create Process then Kill it """
-        self.ram.create_process("Dave", 666, 0)
-        self.assertTrue(self.ram.check_process_exists("Dave"))
-        self.ram.kill("Dave")
-        self.assertFalse(self.ram.check_process_exists("Dave"))
+        ''' Create Process then Kill it '''
+        self.ram.create_process('Dave', 666, 0)
+        self.assertTrue(self.ram.check_process_exists('Dave'))
+        self.ram.kill('Dave')
+        self.assertFalse(self.ram.check_process_exists('Dave'))
 
     def test_get_process_list(self):
-        """ Test Get process List """
-        self.ram.create_process("Style", 200, 0)
-        self.ram.create_process("Red", 333, 111)
-        self.assertEqual(self.ram.process_list, ["Style", "Red"])
+        ''' Test Get process List '''
+        self.ram.create_process('Style', 200, 0)
+        self.ram.create_process('Red', 333, 111)
+        self.assertEqual(self.ram.process_list, ['Style', 'Red'])
 
     def test_find_holes(self):
-        """ Test
+        ''' Test
 
         Note: create_process(process_name, process_size, address)
         M_HIGHT = 450
 
         |----------------------------| Address: 0
-        |    FavoriteThings: 100     |
+        |    FavouriteThings: 100    |
         |----------------------------| Address: 101
         |    Hole: 50                | * Process to big for this hole
         |----------------------------| Address: 151
@@ -61,17 +61,20 @@ class TestMemory(unittest.TestCase):
         |----------------------------| Address: 350
         |    Hole: abs(memory)       | (M_HIGHT - hole address = hole size)
         |----------------------------| (450 - 350 = 100)
-        """
-        self.ram.create_process("FavoriteThings", 100, 0)
-        self.ram.create_process("IWillSurvive", 49, 151)
-        self.ram.create_process("Hello", 99, 251)
+        '''
+        self.ram.create_process('FavoriteThings', 100, 0)
+        self.ram.create_process('IWillSurvive', 49, 151)
+        self.ram.create_process('Hello', 99, 251)
 
-        self.assertEqual(self.ram.find_holes(), [{"address": 100, "size": 51},
-                                                 {"address": 200, "size": 51},
-                                                 {"address": 350, "size": 100}])
+        # Kill Favourite Things to create hole at top of memory
+        self.ram.kill('FavoriteThings')
+
+        self.assertEqual(self.ram.find_holes(), [{'address': 0, 'size': 151},
+                                                 {'address': 200, 'size': 51},
+                                                 {'address': 350, 'size': 100}])
 
     def test_first_fit(self):
-        """ Test First Fit Allocation
+        ''' Test First Fit Allocation
 
         Creating Process with a Size of 110
         * The process is 10 Bytes too big for the first hole
@@ -85,17 +88,17 @@ class TestMemory(unittest.TestCase):
         |----------------------------| Address: 300
         |    Hole: abs(memory)       | * Process should go here
         |----------------------------|
-        """
+        '''
         # Create Processes to simulate real world example
-        self.ram.create_process("FavoriteThings", 99, 0)
-        self.ram.create_process("IWillSurvive", 99, 201)
+        self.ram.create_process('FavoriteThings', 99, 0)
+        self.ram.create_process('IWillSurvive', 99, 201)
 
         # Actually Past Process through First Fit allocation
-        self.ram.first_fit("Summertime", 110)
-        self.assertEqual(self.ram.get_process_address("Summertime"), 300)
+        self.ram.first_fit('Summertime', 110)
+        self.assertEqual(self.ram.get_process_address('Summertime'), 300)
 
     def test_best_fit(self):
-        """ Test Best Fit Allocation
+        ''' Test Best Fit Allocation
 
         Process Size: 110
         * The process should skip the first hole because there may be more
@@ -110,16 +113,16 @@ class TestMemory(unittest.TestCase):
         |----------------------------|
         |    IWillSurvive: 11        |
         |----------------------------| Address: 300
-        """
-        self.ram.create_process("FavoriteThings", 99, 201)
-        self.ram.create_process("IWillSurvive", 11, 431)
+        '''
+        self.ram.create_process('FavoriteThings', 99, 201)
+        self.ram.create_process('IWillSurvive', 11, 431)
 
         # Actually Past Process through Best Fit allocation
-        self.ram.best_fit("Summertime", 110)
-        self.assertEqual(self.ram.get_process_address("Summertime"), 300)
+        self.ram.best_fit('Summertime', 110)
+        self.assertEqual(self.ram.get_process_address('Summertime'), 300)
 
     def test_worst_fit(self):
-        """ Test worst Fit Allocation
+        ''' Test worst Fit Allocation
 
         Process Size: 110
         * The process should skip the first hole because there may be more
@@ -135,16 +138,16 @@ class TestMemory(unittest.TestCase):
         |----------------------------| Address: 346
         |    Hole: 104               |
         |----------------------------|
-        """
-        self.ram.create_process("FavoriteThings", 99, 101)
-        self.ram.create_process("IWillSurvive", 15, 331)
+        '''
+        self.ram.create_process('FavoriteThings', 99, 101)
+        self.ram.create_process('IWillSurvive', 15, 331)
 
         # Actually Past Process through Best Fit allocation
-        self.ram.worst_fit("Summertime", 110)
-        self.assertEqual(self.ram.get_process_address("Summertime"), 200)
+        self.ram.worst_fit('Summertime', 110)
+        self.assertEqual(self.ram.get_process_address('Summertime'), 200)
 
     def test_compact(self):
-        """ Test Compact
+        ''' Test Compact
 
         |----------------------------| Address: 0
         |    Hole: 100               |
@@ -166,11 +169,11 @@ class TestMemory(unittest.TestCase):
         |----------------------------| Address: 116
         |    Hole: N                 |
         |----------------------------|
-        """
-        self.ram.create_process("FavoriteThings", 99, 101)
-        self.ram.create_process("IWillSurvive", 15, 331)
+        '''
+        self.ram.create_process('FavoriteThings', 99, 101)
+        self.ram.create_process('IWillSurvive', 15, 331)
 
         self.ram.compact()
 
-        self.assertEqual(self.ram.get_process_address("FavoriteThings"), 0)
-        self.assertEqual(self.ram.get_process_address("IWillSurvive"), 99)
+        self.assertEqual(self.ram.get_process_address('FavoriteThings'), 0)
+        self.assertEqual(self.ram.get_process_address('IWillSurvive'), 99)
